ad83af8dc873ec6f855a320c5891c98acd64678a
Ilya Krasnov <ilya.krasnov@gmail.com>
2015-08-12 19:55:08 +0200

    Add collection2. Add players schema

---
+++ collections/players/players.js
@@ -0,0 +1,23 @@
+Players = new Mongo.Collection("players");
+
+var Schemas = {};
+
+Schemas.Player = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name",
+        max: 20
+    },
+    points: {
+        type: Number,
+        label: "Points"
+    },
+    registered: {
+        type: Boolean,
+        label: "Registered"
+    },
+    startDate: {
+        type: Date,
+        label: "Start date"
+    }
+});
