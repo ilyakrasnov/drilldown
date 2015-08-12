a0a472a580713f77869d61e9f7bbf2800c27904b
Michael Milde <michael.milde@planifica.de>
2015-08-12 20:58:51 +0200

    tried to include filter-collections. did not work

--- drilldown.js
+++ drilldown.js
@@ -11,10 +11,55 @@ if (Meteor.isClient) {
         }
     });
 
+    PlayersFilter = new Meteor.FilterCollections(Players, {
+        template: 'player'
+            // Other arguments explained later. See Configuration.
+    });
+
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function() {
-        // code to run on server at startup
+        //
+    });
+
+    Meteor.FilterCollections.publish(Players, {
+        name: 'people-lite'
     });
 }
+//
+// PlayersFilter = new Meteor.FilterCollections(Players, {
+//     filters: {
+//         name: {
+//             title: 'name',
+//             operator: ['$regex', 'i'],
+//             condition: '$and',
+//             searchable: true
+//         },
+//         points: {
+//             title: 'points',
+//             operator: ['$eq', '$gte', '$lte'],
+//             condition: '$and',
+//             searchable: true
+//         },
+//         registerd: {
+//             title: 'registered',
+//             operator: ['$eq'],
+//             condition: '$and',
+//             searchable: true
+//         }
+//     }
+// });
+
+PlayersFilter = new Meteor.FilterCollections(Players, {
+    name: 'player-lite',
+    template: 'playersList',
+    filters: {
+        "name": {
+            title: 'Name',
+            operator: ['$regex', 'i'],
+            condition: '$and',
+            searchable: 'required'
+        }
+    }
+});
