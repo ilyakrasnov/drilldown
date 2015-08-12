499b621a5323dc8a52a94e1b3fb9e9f56aede47d
Michael Milde <michael.milde@planifica.de>
2015-08-12 19:32:50 +0200

    added basic functionallity

--- drilldown.js
+++ drilldown.js
@@ -1,23 +1,37 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.helpers({
+        counter: function() {
+            return Session.get('counter');
+        },
+        getPlayer: function() {
+            return players;
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
+
+var players = [{
+    name: "Ilya",
+    points: "100",
+    registered: "true",
+    startDate: "23.03.2015"
+}, {
+    name: "Michi",
+    points: "10",
+    registered: "false",
+    startDate: "15.04.2015"
+}, {
+    name: "Peter",
+    points: "50",
+    registered: "true",
+    startDate: "10.10.2010"
+}];
